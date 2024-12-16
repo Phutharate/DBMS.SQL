623b43df65d2bab6923bcdeb7655744f1f7bd2a6
Phutharate <phutharateheart@gmail.com>
2024-12-16 22:39:10 +0700

    Add project files.

---
+++ DBMS/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace DBMS.SQL.QuarryOrder
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dgvOrders = new DataGridView();
+            dgvDetails = new DataGridView();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)dgvOrders).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dgvDetails).BeginInit();
+            SuspendLayout();
+            // 
+            // dgvOrders
+            // 
+            dgvOrders.AllowUserToAddRows = false;
+            dgvOrders.AllowUserToDeleteRows = false;
+            dgvOrders.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvOrders.Location = new Point(33, 45);
+            dgvOrders.Name = "dgvOrders";
+            dgvOrders.ReadOnly = true;
+            dgvOrders.RowHeadersWidth = 51;
+            dgvOrders.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvOrders.Size = new Size(683, 218);
+            dgvOrders.TabIndex = 0;
+            dgvOrders.CellMouseUp += dgvOrders_CellMouseUp;
+            // 
+            // dgvDetails
+            // 
+            dgvDetails.AllowUserToAddRows = false;
+            dgvDetails.AllowUserToDeleteRows = false;
+            dgvDetails.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvDetails.Location = new Point(33, 302);
+            dgvDetails.Name = "dgvDetails";
+            dgvDetails.ReadOnly = true;
+            dgvDetails.RowHeadersWidth = 51;
+            dgvDetails.Size = new Size(683, 133);
+            dgvDetails.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(33, 12);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(104, 27);
+            textBox1.TabIndex = 2;
+            textBox1.Text = "ใบสั่งซื้อ";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(33, 269);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(96, 27);
+            textBox2.TabIndex = 3;
+            textBox2.Text = "รายการสินค้า";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(dgvDetails);
+            Controls.Add(dgvOrders);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvOrders).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dgvDetails).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dgvOrders;
+        private DataGridView dgvDetails;
+        private TextBox textBox1;
+        private TextBox textBox2;
+    }
+}
